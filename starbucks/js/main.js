a2724d51dcbf360d0db6a83f0c2eb67ac8a44628
Simon Barley <scbarley@syr.edu>
2023-09-04 14:43:04 -0400

    first commit

---
+++ starbucks/js/main.js
@@ -0,0 +1,27 @@
+// console.log("js has been loaded");
+
+// variables, methods, and functions
+
+// 1. Variables
+const myName = "Simon Barley";
+console.log(myName);
+
+const myAge = 22;
+const myCity = "Ithaca";
+
+const introduction = `Hi, my name is ${myName}, I am ${myAge} years old and I live in ${myCity}.`;
+
+console.log(introduction);
+
+
+// 2. methods
+const menuBtn = document.getElementById("menuBtn");
+const overlay = document.getElementById("overlay");
+console.log(menuBtn);
+
+// "event name", callback function
+menuBtn.addEventListener("click", function() {
+    //console.log("menu button has been clicked")
+    overlay.classList.add("active");
+
+}); // end of menuBtn click event
