95f9de64a1d00e4cbcf46594183d95ec9f2ea2e6
Simon Barley <scbarley@syr.edu>
2023-11-30 00:16:32 -0500

    hey professor pasetti, I'm sorry, but all I'll say is rock bottom hit hard, but I finally finished starbucks

--- starbucks/js/coffeeApp.js
+++ starbucks/js/coffeeApp.js
@@ -2,13 +2,16 @@
 const menuBtn = document.getElementById("menuBtn");
 const closeBtn = document.getElementById("closeBtn");
 const overlay = document.getElementById ("overlay");
-const ascendingBtn = document.getElementById("ascendingBtn");
-const descendingBtn = document.getElementById("descendingBtn");
+//const ascendingBtn = document.getElementById("ascendingBtn");
+//const descendingBtn = document.getElementById("descendingBtn");
+const sortBtn = document.getElementById("sortBtn")
+//const sortBtnIcon = document.getElementById("sortBtnIcon")
 const coffeeList = document.getElementById("coffeeList");
 const priceRanges = document.getElementById("priceRanges");
 
 let filteredCoffees = [...coffees];
-let sortDirection = "ascending";
+let sortDirection = "descending";
+
 
 //2. functions
 const buildTextElement = (element, className, content) => {
@@ -25,28 +28,29 @@ const displayList = (arr) => {
     
         //2. create the html elements
         const coffeeArticle = document.createElement("article");
-        coffeeArticle.classList.add("coffee-item");
+        coffeeArticle.classList.add("collection__item");
     
         const coffeeImage = document.createElement("img");
+        coffeeImage.classList.add("collection__item__image");
         coffeeImage.src = `images/${image.fileName}`;
         coffeeImage.width = image.width;
         coffeeImage.height = image.height;
         coffeeImage.alt = image.altText;
     
-        const coffeeTitle = buildTextElement("h2", "coffee-title", title);
+        const coffeeTitle = buildTextElement("h3", "collection__item__title", title);
     
-        const coffeePrice = buildTextElement("h3", "coffee-price", `$${price}`);
+        // const coffeePrice = buildTextElement("h3", "coffee-price", `$${price}`);
     
         // const coffeeDescription = document.createElement("p");
         // coffeeDescription.classList.add("coffee-description");
         // coffeeDescription.textContent = description;
-        const coffeeDescription = buildTextElement("p","coffee-description", description);
+        // const coffeeDescription = buildTextElement("p","coffee-description", description);
     
         //3. append the elements to the parent article
         coffeeArticle.appendChild(coffeeImage);
         coffeeArticle.appendChild(coffeeTitle);
-        coffeeArticle.appendChild(coffeePrice);
-        coffeeArticle.appendChild(coffeeDescription);
+        // coffeeArticle.appendChild(coffeePrice);
+        // coffeeArticle.appendChild(coffeeDescription);
     
         //4. append article to the html document
         coffeeList.appendChild(coffeeArticle);
@@ -67,7 +71,7 @@ const sortListByDirection = (direction, arr) => {
     // console.log({direction});
     sortDirection = direction; 
     const sortedListArr = [...arr].sort((a,b) => {
-        if (direction === "ascending") {
+        if (direction === "descending") {
             if (a.title < b.title) {
                 return -1;
             }
@@ -95,21 +99,45 @@ closeBtn.addEventListener("click", function() {
     overlay.classList.remove("active");
 }); //end of close Btn click event
 
-ascendingBtn.addEventListener("click", function() {
-    //console.log("ascedning clicked");
-    //purgeList();
-    const sortedList = sortListByDirection("ascending", filteredCoffees);
-    //console.log(sortedList);
-    displayList(sortedList);
-}); // end of ascending  button click event
 
-descendingBtn.addEventListener("click", function() {
-    //console.log("descending clicked");
-    //purgeList();
-    const sortedList = sortListByDirection("descending", filteredCoffees);
-    //console.log(sortedList);
+let isDescending = true;
+
+sortBtn.addEventListener("click", () => {
+    //bang operator (!) flips boolean value
+    isDescending = !isDescending;
+
+    //ternary statement
+    let direction = isDescending ? "descending" : "ascending";
+    const sortedList = sortListByDirection(direction, filteredCoffees);
     displayList(sortedList);
-}); //end of descending button click event
+
+    sortBtn.innerHTML = "";
+    
+    const sortBtnIcon = document.createElement("img");
+    sortBtnIcon.src = `images/arrow-${direction}.svg`;
+    //sortBtnIcon.attr("src", `images/arrow-${direction}.svg`);
+
+    sortBtn.appendChild(sortBtnIcon);
+    //sortBtnIcon.attributes("src", "")
+    //sortBtnIcon.attr("src", `images/arrow-${direction}.svg`)
+});
+
+
+// ascendingBtn.addEventListener("click", function() {
+//     //console.log("ascedning clicked");
+//     //purgeList();
+//     const sortedList = sortListByDirection("ascending", filteredCoffees);
+//     //console.log(sortedList);
+//     displayList(sortedList);
+// }); // end of ascending  button click event
+
+// descendingBtn.addEventListener("click", function() {
+//     //console.log("descending clicked");
+//     //purgeList();
+//     const sortedList = sortListByDirection("descending", filteredCoffees);
+//     //console.log(sortedList);
+//     displayList(sortedList);
+// }); //end of descending button click event
 
 priceRanges.addEventListener("change", (event) => {
     console.log("price range changed");
